56e6b0c7c6a132fd9edbe4127c4bd8754c24ad67
Pedro-ETS <pedrotorrestec@gmail.com>
2024-04-12 07:55:17 -0500

    update carrito de compras

--- controllers/carts.js
+++ controllers/carts.js
@@ -3,38 +3,25 @@ const { HttpStatus, HttpResponseMessage } = require("../enums/http");
 
 exports.addToCart = async (req, res, next) => {
   try {
-    const {cardId, quantity } = req.body;
+    const {cardId} = req.params;
     const userId = req.user._id;
-      let cart = await cartModel.findOne({ userId }).populate('products.cardId');//populate para extraer los datos del producto
-    if (!cart) {// Si no existe un carrito para este usuario, lo creamos
+      let cart = await cartModel.findOne({ userId });//populate para extraer los datos del producto
+
+      if (!cart) {// Si no existe un carrito para este usuario, lo creamos
       cart = new cartModel ({ userId, products: [] });
     }
-    const productIndex = cart.products.findIndex(product => product.cardId.toString() === cardId);// Verificamos si el producto ya está en el carrito
-    if (productIndex !== -1) {
-      // Si el producto ya está en el carrito, actualizamos la cantidad
-      cart.products[productIndex].quantity += quantity;
+    const productIndex = cart.products.findIndex(product => product.cardId._id.toString() === cardId);// Verificamos si el producto ya está en el carrito
+    if (productIndex !== -1) {// Si el producto ya está en el carrito, actualizamos la cantidad
+      cart.products[productIndex].quantity += 1;
     } else {
-      // Si el producto no está en el carrito, lo agregamos
-      cart.products.push({ cardId, quantity });
+
+      cart.products.push({ cardId, quantity:1 });// Si el producto no está en el carrito, lo agregamos
+
     }
     await cart.save();
 
-    // Formateamos la respuesta para que sea consistente con el formato de getCartData
-    const cartData = {
-      userId: cart.userId,
-      products: cart.products.map(product => ({
-        product: {
-          _id: product.cardId._id,
-          name: product.cardId.name,
-          price: product.cardId.price,
-          stock: product.cardId.stock,
-          // Agregar más campos según sea necesario
-        },
-        quantity: product.quantity
-      }))
-    };
 
-    res.send({ data: cartData });
+    res.send({ data: cart });
   } catch (error) {
     if (error.name === "ValidationError") {
       return res.status(HttpStatus.BAD_REQUEST).send({ error: "se pasaron datos invalidos al agregar un producto" });
@@ -43,14 +30,45 @@ exports.addToCart = async (req, res, next) => {
   }
 };
 
+exports.subtractFromCartQuantity = async (req, res, next) => {
+  try {
+    const { cardId} = req.params;
+    const userId = req.user._id;
+    console.log("infomacion del Id");
+    console.log(cardId);
+    let cart = await cartModel.findOne({ userId });
+
+    if (!cart) {
+      return res.status(HttpStatus.NOT_FOUND).send({ error: "No se encontró el carrito para este usuario" });
+    }
+
+    const productIndex = cart.products.findIndex(product => product.cardId._id.toString() === cardId);
+
+    if (productIndex !== -1) {
+      cart.products[productIndex].quantity -= 1;
+
+      // Si la cantidad después de restar es menor o igual a cero, eliminar el producto del carrito
+      if (cart.products[productIndex].quantity <= 0) {
+        cart.products.splice(productIndex, 1);
+      }
+
+      await cart.save();
+      res.send({ message: "Cantidad del producto actualizada exitosamente en el carrito" });
+    } else {
+      res.status(HttpStatus.NOT_FOUND).send({ error: "El producto no está en el carrito" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Controlador para obtener los datos del carrito y la información del producto asociado
-exports.getCartData =  async  (req, res,next) => {
+exports.getCartData =  async  (req, res, next) => {
   try {
       const userId = req.user._id;
 
       // Buscar el carrito del usuario en la base de datos
       const cart = await cartModel.findOne({ userId }).populate('products.cardId');
-
       if (!cart) {
         return res.status(HttpStatus.OK).json({ message: 'No hay elementos en el carrito para este usuario.' });
       }
@@ -69,13 +87,12 @@ exports.getCartData =  async  (req, res,next) => {
               quantity: product.quantity
           }))
       };
-
       // Enviar la respuesta con los datos del carrito y los productos asociados
       res.send({ data: cartData });
   }catch (error) {
     next(error);
   }
-}
+};
 
 
 
@@ -109,32 +126,32 @@ exports.updateCart = async (req, res, next) => {
 };
 
 // Controlador para eliminar un producto del carrito
-exports.removeFromCart = async (req, res) => {
-  try {
-    const cardId = req.params.cardId;
-    const userId = req.user._id;
+// exports.removeFromCart = async (req, res) => {
+//   try {
+//     const cardId = req.params.cardId;
+//     const userId = req.user._id;
 
-    let cart = await cartModel.findOne({ userId });
+//     let cart = await cartModel.findOne({ userId });
 
-    if (!cart) {
-      return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Carrito no encontrado' });
-    }
+//     if (!cart) {
+//       return res.status(HttpStatus.BAD_REQUEST).json({ error: 'Carrito no encontrado' });
+//     }
 
-    const productIndex = cart.products.findIndex(product => product.cardId.toString() === cardId);
+//     const productIndex = cart.products.findIndex(product => product.cardId.toString() === cardId);
 
-    if (productIndex !== -1) {
-      // Si el producto está en el carrito, lo eliminamos
-      cart.products.splice(productIndex, 1);
-      await cart.save();
-      res.status(200).json(cart);
-    } else {
-      // Si el producto no está en el carrito, devolvemos un error
-      res.status(HttpStatus.BAD_REQUEST).json({ error: 'Producto no encontrado en el carrito' });
-    }
-  }catch (error) {
-    if (error.name === "ValidationError") {
-      return res.status(HttpStatus.BAD_REQUEST).send({ error: "se pasaron datos invalidos al eliminar un producto" });
-    }
-    next(error);
-  }
-};
+//     if (productIndex !== -1) {
+//       // Si el producto está en el carrito, lo eliminamos
+//       cart.products.splice(productIndex, 1);
+//       await cart.save();
+//       res.status(200).json(cart);
+//     } else {
+//       // Si el producto no está en el carrito, devolvemos un error
+//       res.status(HttpStatus.BAD_REQUEST).json({ error: 'Producto no encontrado en el carrito' });
+//     }
+//   }catch (error) {
+//     if (error.name === "ValidationError") {
+//       return res.status(HttpStatus.BAD_REQUEST).send({ error: "se pasaron datos invalidos al eliminar un producto" });
+//     }
+//     next(error);
+//   }
+// };
--- routes/carts.js
+++ routes/carts.js
@@ -7,43 +7,57 @@ const {
   addToCart,
   updateCart,
   removeFromCart,
-  getCartData
+  getCartData,
+  subtractFromCartQuantity
 } = require("../controllers/carts");
 
 const authHeaderSchema = Joi.object({
   authorization: Joi.string().required(),
 }).unknown(true);
 
-router.post("/",celebrate({//crea un carrito para el usuario si no tiene uno y agrega productos al carrito
-  body: Joi.object().keys({
-    cardId: Joi.string().required(),
-    quantity: Joi.number().integer().min(1).max(30).required(),
-    headers: authHeaderSchema,
-  })
+router.post("/:cardId/quantity",celebrate({// agrega productos
+  params: {
+    cardId: Joi.string().required()
+  },
 }), addToCart);
 
-router.get("/", celebrate({
+router.get("/", celebrate({//obtiene el carrito donde estan todos los productos del usuario
   headers: authHeaderSchema
 }), getCartData);
 
-router.post("/:cardId/quantity",celebrate({//actualiza cantidad a comprar de cada producto
-  body: Joi.object().keys({
-    quantity: Joi.number().integer().min(1).max(30).required(),
-    headers: authHeaderSchema,
-  }),
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24).required(),
-  }),
-  headers: authHeaderSchema,
-}), updateCart);
 
-router.delete("/:cardId/product", celebrate({//elimina productos del carrito
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24).required(),
-    headers: authHeaderSchema,
-  }),
+
+
+router.delete("/:cardId/quantity",celebrate({//elimina cantidad a comprar de producto
+  params: {
+    cardId: Joi.string().required()
+  },
   headers: authHeaderSchema,
-}), removeFromCart);
+}), subtractFromCartQuantity);
+
+
+
+
+
+
+// router.post("/:cardId/quantity",celebrate({//actualiza cantidad a comprar de cada producto
+//   body: Joi.object().keys({
+//     quantity: Joi.number().integer().min(1).max(30).required(),
+//     headers: authHeaderSchema,
+//   }),
+//   params: Joi.object().keys({
+//     cardId: Joi.string().alphanum().length(24).required(),
+//   }),
+//   headers: authHeaderSchema,
+// }), updateCart);
+
+// router.delete("/:cardId/product", celebrate({//elimina productos del carrito
+//   params: Joi.object().keys({
+//     cardId: Joi.string().alphanum().length(24).required(),
+//     headers: authHeaderSchema,
+//   }),
+//   headers: authHeaderSchema,
+// }), removeFromCart);
 
 
 module.exports = router;
